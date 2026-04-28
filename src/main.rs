1ff71bc9a14aa3b300970bb32ffe70cf8193258e
KeksNino <KeksNino@proton.me>
2026-04-28 21:11:51 +0200

    Add Files

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+mod rutracker;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    rutracker::main();
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()?;
+
+    Ok(())
+}
---
+++ src/rutracker.rs
@@ -0,0 +1,3 @@
+pub fn main() {
+    //let results = reqwest::get("")
+}
